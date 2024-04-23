a81f4882350bd915981fa61ca6a7f7de983e0750
Tito Alexandre Barbosa Pereira <tab.pereira00@gmail.com>
2024-04-23 13:44:06 +0100

    sketch

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tibarbos <tibarbos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 16:47:43 by tibarbos          #+#    #+#             */
-/*   Updated: 2024/04/22 17:01:43 by tibarbos         ###   ########.fr       */
+/*   Updated: 2024/04/23 13:24:57 by tibarbos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,41 @@
 
 /*
 Arguments:
-- number_of_philosophers
-- time_to_die
-- time_to_eat
-- time_to_sleep
-- number_of_times_each_philosopher_must_eat (optional)
+- av[0] number_of_philosophers
+- av[1] time_to_die
+- av[2] time_to_eat
+- av[3] time_to_sleep
+- av[4] number_of_times_each_philosopher_must_eat (optional)
+*/
+
+// cada thread individual
+void	the_philo(t_all *all)
+{
+	// stuff;
+	while (i != times_to_eat)
+	{
+		ez;
+	}
+}
+/*
+por default, inicializar o times to eat para -1
+assim, caso nao haja a opcao, essa condicao vai ser sempre falsa e,
+caso haja opcao, eventualmente vai ser verdadeira
 */
 
 int	main(int ac, char **av)
 {
-	//stuff;
+	t_all	*all;
+
+	if (ac == 5 || (ac == 6 && av[6]))
+	{
+		all = malloc(sizeof(t_all));
+		create_all(av, &all);
+		all->begin_time = gettimeofday();
+		all->eat_no = -1;
+		if (av[4])
+			all.eat_no = av[4];
+	}
+	else
+		printf("Wrong number or invalid arguments.\n");
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: tibarbos <tibarbos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 16:48:12 by tibarbos          #+#    #+#             */
-/*   Updated: 2024/04/22 16:49:16 by tibarbos         ###   ########.fr       */
+/*   Updated: 2024/04/23 13:24:07 by tibarbos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+/*
+typedef struct	s_times() {
+	size_t	time_to_die;
+	size_t	time_to_eat;
+	size_t	time_to_sleep;
+}   t_times;*/
+
+typedef struct	s_all
+{
+	int philo_num;
+	int *forks;
+	//t_times *times;
+	size_t	begin_time;
+    size_t	time_to_die;
+	size_t	time_to_eat;
+	size_t	time_to_sleep;
+}   t_all;
 
 #endif
